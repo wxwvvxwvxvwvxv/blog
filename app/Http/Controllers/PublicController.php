2a510e3fd7972c0fb5f8dbe0244181af8fec929f
Diana Pronina <diana.pronina6c@gmail.com>
2023-11-15 11:29:22 +0200

    add

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function index(){
+        $posts = Post::all();
+        //dd($posts->toArray());
+        return view('welcome', compact('posts'));
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function snippet():Attribute {
+        return Attribute::get(function (){
+            return substr($this->body, 0, 200);
+        });
+    }
+}
